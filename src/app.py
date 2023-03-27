e9d36f59f9165b2c549e96eff9d15391f91106a4
OVYED Deno <nevad34@gmail.com>
2023-03-27 19:15:21 +0000

    3/27

--- src/app.py
+++ src/app.py
@@ -25,27 +25,13 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
-
-
-    return jsonify(response_body), 200
-
 @app.route('/members', methods=['POST'])
 def add_members():
 
     # this is how you can use the Family datastructure by calling its methods
     member = request.json
-    jackson_family.add_member(member)
-    return josonify (member), 200
+    members= jackson_family.add_member(member)
+    return jsonify (members), 200
 
 @app.route('/members/<int:id>', methods=['DELETE'])
 def delete_member(id):
@@ -61,8 +47,8 @@ def get_member(id):
     member = jackson_family.get_member(id)
     return jsonify(member), 200
 
-@app.route('/members/<int:id>', methods=['GET'])
-def get_all_members(id):
+@app.route('/members', methods=['GET'])
+def get_all_members():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
